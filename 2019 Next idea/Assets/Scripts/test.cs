671167b24cdfe0ea3b0e9db3050b124e63a03532
xqqxiaoqiqi <49115834+xqqxiaoqiqi@users.noreply.github.com>
2019-07-02 19:59:20 +0800

    Update test.cs

--- 2019 Next idea/Assets/Scripts/test.cs
+++ 2019 Next idea/Assets/Scripts/test.cs
@@ -8,6 +8,7 @@ public class test : UnitySingleton<test>
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("0.0");
     }
 
     // Update is called once per frame
