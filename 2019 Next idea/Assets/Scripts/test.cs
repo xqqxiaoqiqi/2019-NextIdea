8b42fc02c63658687dbf32b85ab2a7999912c0c6
xqqxiaoqiqi <49115834+xqqxiaoqiqi@users.noreply.github.com>
2019-07-02 20:00:11 +0800

    Update test.cs

--- 2019 Next idea/Assets/Scripts/test.cs
+++ 2019 Next idea/Assets/Scripts/test.cs
@@ -8,6 +8,7 @@ public class test : UnitySingleton<test>
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("commit conflict");
     }
 
     // Update is called once per frame
