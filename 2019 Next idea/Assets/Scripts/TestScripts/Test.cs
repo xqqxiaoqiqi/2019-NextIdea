44b82ec74cb5b7dd33a26cabba0474cc9760a3d9
xqqxiaoqiqi <49115834+xqqxiaoqiqi@users.noreply.github.com>
2019-07-11 16:45:23 +0800

    Update Test.cs

--- 2019 Next idea/Assets/Scripts/TestScripts/Test.cs
+++ 2019 Next idea/Assets/Scripts/TestScripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("master");
+        Debug.Log("master1");
     }
 
     // Update is called once per frame
