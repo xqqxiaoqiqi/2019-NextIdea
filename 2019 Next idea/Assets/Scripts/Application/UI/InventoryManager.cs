b7f5b9bb7cb3ffb2382be6feca26470480ae0263
CMSherlock <1164407295@qq.com>
2019-07-14 15:54:26 +0800

    部分功能模块整合
    完成场景拖动功能、缩放功能、以及拖动放置元件的功能，但是目前选中元件的操作会与拖动元件操作发生冲突，暂时不将选中后弹出按钮的代码上传。界面的退出，跳转等按钮相关函数已经写好，后面搭建场景时，可以通知我来给他们挂上去。同时，请将所有的Prefeb更改为Sprite谢谢，这些不是UI元素，不需要用Button或者Image。
    冲突的问题我会继续看的。完成后也会上传。
    目前创建了一个SceneManager函数，统一管理关卡跳转等切换场景的操作。但由于现在不清楚最后的场景配置。因此暂不上传。
    冲突的问题这两天我会尽快解决，完成后会及时上传。

---
+++ 2019 Next idea/Assets/Scripts/Application/UI/InventoryManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    public Transform ElementManger;
+
+    public CameraOperation cameraManager;
+
+    public GameObject WirePhysicPrefeb;
+
+    [HideInInspector] public bool IsDraggingElement = false;
+
+}
--- 2019 Next idea/Assets/Scripts/TestScripts/ShowMessage.cs
+++ 2019 Next idea/Assets/Scripts/TestScripts/ShowMessage.cs
@@ -8,29 +8,29 @@ public class ShowMessage : MonoBehaviour
 {
     public void ShowDetail()
     {
-        Debug.Log("充能状态：" + GetComponent<BaseLand>().hascharged);
+       // Debug.Log("充能状态：" + GetComponent<BaseLand>().hascharged);
         GetSourcesMessage();
 
     }
     public void GetSourcesMessage()
     {
-        Debug.Log("可用地格充能源（元件）：");
+        //Debug.Log("可用地格充能源（元件）：");
         for (int i = 0; i < GetComponent<BaseLand>().inputelements.Count; i++)
         {
             if (GetComponent<BaseLand>().inputelements[i] != null)
             {
-                Debug.Log(GetComponent<BaseLand>().inputelements[i]);
+                //Debug.Log(GetComponent<BaseLand>().inputelements[i]);
             }
             else
             {
-                Debug.Log("none");
+              //  Debug.Log("none");
             }
 
         }
 
         if (GetComponentInChildren<Element>() != null)
         {
-            Debug.Log("元件激活源（地格）：");
+            //Debug.Log("元件激活源（地格）：");
             if (GetComponentInChildren<Element>().landsource!=null)
             {
                 //for(int j=0; j< GetComponentInChildren<Element>().landsources.Count; j++)
@@ -42,9 +42,9 @@ public class ShowMessage : MonoBehaviour
             }
             else
             {
-                Debug.Log("none");
+               // Debug.Log("none");
             }
-            Debug.Log("元件激活地格（输出）：");
+           // Debug.Log("元件激活地格（输出）：");
 
             if (GetComponentInChildren<Element>().outputlands != null)
             {
@@ -58,7 +58,7 @@ public class ShowMessage : MonoBehaviour
         }
         else
         {
-            Debug.Log("地格上没有元件");
+           // Debug.Log("地格上没有元件");
         }
     }
 }
