051279d683fa02c6bd93e6ff00946c929e105e1c
xqqxiaoqiqi <49115834+xqqxiaoqiqi@users.noreply.github.com>
2019-07-24 20:39:55 +0800

    prefab修改

---
+++ 2019 Next idea/Assets/Scripts/Application/BasicElements/Light.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameTool
+{
+    public class Light : Element
+    {
+        private void Awake()
+        {
+            element_ID = "light";
+        }
+        public override void UpdateTexture()
+        {
+            GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load(enable_texturepath + element_ID, typeof(Sprite));
+            GetComponent<ElementParticle>().UpdateParticleType(element_ID);
+        }
+
+    }
+}
+
